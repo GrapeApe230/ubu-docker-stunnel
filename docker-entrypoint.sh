0a9d65dd513d59245d4e59d6ce7d818eba2cb310
pizzim13 <39072577+pizzim13@users.noreply.github.com>
2020-05-27 13:23:13 -0400

    Add files via upload
    changed to builder

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -18,11 +18,4 @@ accept = ${ACCEPT}
 connect = ${CONNECT}
 _EOF_
 
-if ! [ -f stunnel.pem ]
-then
-    openssl req -x509 -nodes -newkey rsa:2048 -days 3650 -subj '/CN=stunnel' \
-                -keyout stunnel.pem -out stunnel.pem
-    chmod 600 stunnel.pem
-fi
-
 exec stunnel "$@"
