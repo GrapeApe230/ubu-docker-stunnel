ad8a2afe1a19df7b588875ac9d90acb61b60080d
Peter P Pizzimenti <pppizzim@syr.edu>
2020-05-28 20:12:27 -0400

    undid most changes

--- Dockerfile
+++ Dockerfile
@@ -4,10 +4,7 @@ RUN apt-get update \
     openssl \
     stunnel4 \
     && rm -rf /var/lib/apt/lists/*
-WORKDIR /stunnel
+VOLUME [ "/etc/stunnel" ]
 COPY docker-entrypoint.sh .
-RUN chmod +x /stunnel/docker-entrypoint.sh \
-&& chown -R stunnel4:stunnel4 /stunnel \
-&& chmod -R 755 /stunnel
-USER stunnel4
-ENTRYPOINT [ "/stunnel/docker-entrypoint.sh" ]
+RUN chmod +x docker-entrypoint.sh 
+ENTRYPOINT [ "/docker-entrypoint.sh" ]
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -12,8 +12,11 @@ CN = ${CN}
 _EOF_
 
 openssl req -x509 -nodes -new -newkey rsa:2048 -days 3650 -config openssl.cnf -keyout stunnel.pem -out stunnel.pem
-chmod 600 stunnel.pem
+mv stunnel.pem /etc/ssl/private/
+chmod 600 /etc/ssl/private/stunnel.pem
 rm openssl.cnf
+
+cd /etc/stunnel
 
 cat > stunnel.conf <<_EOF_
 foreground = yes
@@ -21,7 +24,7 @@ setuid = stunnel4
 setgid = stunnel4
 socket = l:TCP_NODELAY=1
 socket = r:TCP_NODELAY=1
-cert = /stunnel/stunnel.pem
+cert = /etc/ssl/private/stunnel.pem
 client = ${CLIENT:-no}
 pid = /var/run/stunnel4/stunnel.pid
 
@@ -30,4 +33,4 @@ accept = ${ACCEPT}
 connect = ${CONNECT}
 _EOF_
 
-exec stunnel "/stunnel/stunnel.conf"
+exec stunnel "$@"
