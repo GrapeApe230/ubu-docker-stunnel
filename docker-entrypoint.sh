ab0d343f85e28b6a712189bce6f8af3730749a94
Peter P Pizzimenti <pppizzim@syr.edu>
2020-06-01 14:37:00 -0400

    changed paths

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-cd /stunnel
 
 touch openssl.cnf
 
@@ -13,11 +12,8 @@ CN = ${CN}
 _EOF_
 
 openssl req -x509 -nodes -new -newkey rsa:2048 -days 3650 -config openssl.cnf -keyout stunnel.pem -out stunnel.pem
-#mv stunnel.pem /etc/ssl/private/
 chmod 600 stunnel.pem
 rm openssl.cnf
-
-#cd /etc/stunnel
 
 cat > stunnel.conf <<_EOF_
 foreground = yes
@@ -25,7 +21,7 @@ setuid = stunnel4
 setgid = stunnel4
 socket = l:TCP_NODELAY=1
 socket = r:TCP_NODELAY=1
-cert = /stunnel.pem
+cert = /stunnel/stunnel.pem
 client = ${CLIENT:-no}
 pid = /var/run/stunnel4/stunnel.pid
 
@@ -34,4 +30,4 @@ accept = ${ACCEPT}
 connect = ${CONNECT}
 _EOF_
 
-exec stunnel "/stunnel/stunnel.conf"
+exec stunnel "stunnel.conf"
